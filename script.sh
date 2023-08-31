60bfb6203331e9e17382157c5f5be75b38a9b1c3
unknown <tunglies@outlook.com>
2023-08-31 17:43:27 +0800

    change: workflow update

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 cache_dir=".cache"
 cache_file="$cache_dir/cache.txt"
+output_file="output/all.txt"
 for folder in $(find $cache_dir -type d -maxdepth 1 ! -name $cache_dir);
 do
     for file in $(
@@ -14,4 +15,4 @@ do
         echo)  >> $cache_file
     done
 done
-awk NF $cache_file | sort | uniq > all.txt
+awk NF $cache_file | sort | uniq > $output_file
