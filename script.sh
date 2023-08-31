7ef76e1d108910166f8e44b93bb311c0bcbc5d84
unknown <tunglies@outlook.com>
2023-08-31 17:45:13 +0800

    fix: Broken pipe

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 cache_dir=".cache"
 cache_file="$cache_dir/cache.txt"
 output_file="output/all.txt"
+
 for folder in $(find $cache_dir -type d -maxdepth 1 ! -name $cache_dir);
 do
     for file in $(
@@ -15,4 +16,6 @@ do
         echo)  >> $cache_file
     done
 done
+
+mkdir output
 awk NF $cache_file | sort | uniq > $output_file
