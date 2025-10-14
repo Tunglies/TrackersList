f92243eae2999f21804604409dcb1508c90e6ec6
Tunglies <77394545+Tunglies@users.noreply.github.com>
2025-10-14 22:00:03 +0800

    fix: correct syntax and formatting in script.sh and all.txt #9, #10

--- script.sh
+++ script.sh
@@ -1,17 +1,21 @@
 cache_dir=".cache"
 cache_file="$cache_dir/cache.txt"
-for folder in $(find $cache_dir -type d -maxdepth 1 ! -name $cache_dir);
-do
+> "$cache_file"
+
+for folder in $(find "$cache_dir" -type d -maxdepth 1 ! -name "$cache_dir"); do
     for file in $(
-        ls -tr $folder/*.txt |
+        ls -tr "$folder"/*.txt 2>/dev/null |
         grep -i -v aria |
-        grep -i -v blacklist);
-    do
+        grep -i -v blacklist
+    ); do
         (
-        awk NF $file | sort | uniq |
-        sed '/#/d' |
-        grep -i -E ^"http|udp";
-        echo)  >> $cache_file
+            awk NF "$file" | sort | uniq |
+            sed '/#/d' |
+            sed 's/"$//' |
+            grep -i -E '^http|udp'
+            echo
+        ) >> "$cache_file"
     done
 done
-awk NF $cache_file | sort | uniq > all.txt
+
+awk NF "$cache_file" | sort | uniq > all.txt
